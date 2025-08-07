f063465ff556c21df687f66581920e6795d305d0
LJKS <lschmid@uos.de>
2025-08-07 14:58:42 +0200

    push search_node gating mechanism untested

--- src/autora/theorist/teammate/l0_layer.py
+++ src/autora/theorist/teammate/l0_layer.py
@@ -38,12 +38,27 @@ class LZeroGate(nn.Module):
                 )
             x = x * z
             # In PyTorch, add regularization loss manually to your total loss
-            self._last_reg_loss = self.regularization_loss()
+            # self._last_reg_loss = self.regularization_loss()
             return x
         else:
             return x
 
-    def hard_concrete_cdf(self, s_dash):
+    def get_expected_gate(self):
+        z = torch.round(
+            torch.clamp(
+                torch.sigmoid(self.log_alpha) * (self.zeta - self.gamma) + self.gamma,
+                0.0, 1.0
+            )
+        )
+        return z
+
+    def open_gate_probability(self):
+        ones = torch.ones(self.shape, device=self.log_alpha.device)
+        probs_one = self.stretched_concrete_cdf(ones)
+        return probs_one
+
+
+    def stretched_concrete_cdf(self, s_dash):
         s = (s_dash - self.gamma) / (self.zeta - self.gamma)
         cdf = torch.sigmoid(
             (torch.log(s) - torch.log(1 - s)) * self.beta - self.log_alpha
@@ -52,22 +67,22 @@ class LZeroGate(nn.Module):
 
     def regularization_loss(self):
         if LOSS_MODE == "binary_weighted":
-            p_leq_0 = self.hard_concrete_cdf(torch.tensor(0., device=self.log_alpha.device))
-            p_geq_1 = 1 - self.hard_concrete_cdf(torch.tensor(1., device=self.log_alpha.device))
+            p_leq_0 = self.stretched_concrete_cdf(torch.tensor(0., device=self.log_alpha.device))
+            p_geq_1 = 1 - self.stretched_concrete_cdf(torch.tensor(1., device=self.log_alpha.device))
             sum_binary = P_ZERO_LOSS_FACTOR * p_leq_0 + P_ONE_LOSS_FACTOR * p_geq_1
             binarization_loss = -sum_binary
             return torch.mean(binarization_loss)
         elif LOSS_MODE == "prob_target_MSE":
-            p_leq_0 = self.hard_concrete_cdf(torch.tensor(0., device=self.log_alpha.device))
-            p_geq_1 = 1 - self.hard_concrete_cdf(torch.tensor(1., device=self.log_alpha.device))
+            p_leq_0 = self.stretched_concrete_cdf(torch.tensor(0., device=self.log_alpha.device))
+            p_geq_1 = 1 - self.stretched_concrete_cdf(torch.tensor(1., device=self.log_alpha.device))
             loss_leq = (torch.mean(p_leq_0) - BINARY_MSE_TARGETS[0]) ** 2
             loss_geq = (torch.mean(p_geq_1) - BINARY_MSE_TARGETS[1]) ** 2
             binarization_loss = loss_leq + loss_geq
             return binarization_loss
         elif LOSS_MODE == "lagrangian_prob":
-            p_geq_1 = torch.mean(1 - self.hard_concrete_cdf(torch.tensor(1., device=self.log_alpha.device)))
+            p_geq_1 = torch.mean(1 - self.stretched_concrete_cdf(torch.tensor(1., device=self.log_alpha.device)))
             geq_1_loss = LAGRANGIAN_P_1_TARGET - p_geq_1
-            p_leq_0 = torch.mean(self.hard_concrete_cdf(torch.tensor(0., device=self.log_alpha.device)))
+            p_leq_0 = torch.mean(self.stretched_concrete_cdf(torch.tensor(0., device=self.log_alpha.device)))
             p_non_binary = 1.0 - (p_geq_1 + p_leq_0)
             non_binary_loss = p_non_binary - LAGRANGIAN_NONBINARY_MAX
             loss_vector = torch.stack((geq_1_loss, non_binary_loss))
--- src/autora/theorist/teammate/search_node.py
+++ src/autora/theorist/teammate/search_node.py
@@ -1,6 +1,36 @@
 import torch
 from torch import nn
+import l0_layer
 
 COMPONENT_DICT = {}
-
+REDUCE_OPS = {'sum': lambda x: torch.sum(x, dim=1)}
+REDUCE_OPS_NOTATION = {'sum': '+'}
 class SearchNode(nn.Module):
+    def __init__(self, components: list, reduce_op='sum'):
+        '''
+        args:
+            components: list of strings (keys in COMPONENT_DICT) components to be used in the search node
+        '''
+        super().__init__()
+        self.component_strings = components
+        self.components = [COMPONENT_DICT[component]() for component in components]
+        self.size = len(self.components)
+        self.gates = l0_layer.LZeroGate((self.size,))
+        assert reduce_op in REDUCE_OPS, f"Invalid reduce operation: {reduce_op}. Available operations: {list(REDUCE_OPS.keys())}"
+        self.reduce_op = REDUCE_OPS[reduce_op]
+        assert reduce_op in REDUCE_OPS_NOTATION, f"Invalid reduce operation notation: {reduce_op}. Available notations: {list(REDUCE_OPS_NOTATION.keys())}"
+        self.reduce_op_notation = REDUCE_OPS_NOTATION[reduce_op]
+
+
+    def forward(self, independent_variables, gate_activated=True, test=False):
+        """
+        Forward pass through the search node, applying each component in parallel
+        args:
+            independent_variables: input data (batched) to be processed by the equation components
+        """
+        component_results = [component(independent_variables) for component in self.components] #shape: (batch_size, num_components)
+        weighted_component_results = self.gates(component_results)
+        output = self.reduce_op(weighted_component_results)
+        return output
+
+    def to_equation(self):
